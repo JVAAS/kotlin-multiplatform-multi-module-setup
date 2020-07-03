dd40b8e99ba0e2edf5bcbcf079bd416e2732144d
vlad <jan@jvaas.com>
2020-07-03 12:51:34 +0200

    restored multiplatform mode.

--- modules/module-mp-1/build.gradle.kts
+++ modules/module-mp-1/build.gradle.kts
@@ -1,41 +1,103 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackOutput.Target.UMD
 
 plugins {
+	val kotlinVersion = "1.4-M2"
 	kotlin("jvm") version "1.4-M2"
+	kotlin("multiplatform") version kotlinVersion
 }
 group = "com.generic.modules"
 version = "1.0.0"
 
 repositories {
-	mavenCentral()
 	jcenter()
+	mavenCentral()
 	maven {
 		url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
 	}
+	maven {
+		url = uri("https://kotlin.bintray.com/kotlinx")
+	}
+	gradlePluginPortal()
 }
-dependencies {
-	testImplementation(kotlin("test-junit5"))
-	implementation(kotlin("stdlib-jdk8"))
 
-	// ktor
-	val ktorVersion = "1.3.1"
-	//implementation("io.ktor:ktor-server-netty:$ktorVersion")
-	implementation("io.ktor:ktor-server-cio:$ktorVersion")
-	implementation("io.ktor:ktor-html-builder:$ktorVersion")
-	implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.1")
+kotlin {
 
-	// logging
-	val slf4jVersion = "1.7.25"
-	val logbackVersion = "1.2.3"
-	implementation("org.slf4j:slf4j-api:$slf4jVersion")
-	implementation("org.slf4j:jcl-over-slf4j:$slf4jVersion")
-	implementation("org.slf4j:jul-to-slf4j:$slf4jVersion")
-	implementation("org.slf4j:log4j-over-slf4j:$slf4jVersion")
-	implementation("ch.qos.logback:logback-classic:$logbackVersion")
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = "11"
+		}
+	}
+	js(IR) {
+		browser {
+			binaries.executable()
+			webpackTask {
+				output.libraryTarget = UMD
+				//output.libraryTarget = COMMONJS
+				//output.libraryTarget = AMD
+			}
+		}
+	}
 
-	api(project(":integrations:integration-jvm-1"))
+	sourceSets {
+
+		val serializationVersion = "0.20.0-1.4-M2"
+		val coroutinesVersion = "1.3.7-1.4-M2"
+		val klockVersion = "1.11.12"
+
+		val commonMain by getting {
+			dependencies {
+				implementation(kotlin("stdlib-common"))
+				implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion")
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test-common"))
+				implementation(kotlin("test-annotations-common"))
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				implementation(kotlin("stdlib-jdk8"))
+				implementation(kotlin("reflect"))
+
+				// logging
+				val slf4jVersion = "1.7.25"
+				val logbackVersion = "1.2.3"
+				implementation("org.slf4j:slf4j-api:$slf4jVersion")
+				implementation("org.slf4j:jcl-over-slf4j:$slf4jVersion")
+				implementation("org.slf4j:jul-to-slf4j:$slf4jVersion")
+				implementation("org.slf4j:log4j-over-slf4j:$slf4jVersion")
+				implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+				// integration
+				api(project(":integrations:integration-jvm-1"))
+
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(kotlin("test-junit"))
+			}
+		}
+		val jsMain by getting {
+			dependencies {
+				implementation(kotlin("stdlib-js"))
+			}
+		}
+		val jsTest by getting {
+			dependencies {
+				implementation(kotlin("test-js"))
+			}
+		}
+		all {
+			languageSettings.enableLanguageFeature("InlineClasses")
+		}
+	}
 
 }
-tasks.withType<KotlinCompile>() {
-	kotlinOptions.jvmTarget = "11"
-}
+
+//tasks.withType<KotlinCompile>() {
+//	kotlinOptions.jvmTarget = "11"
+//	kotlinOptions.js.
+//}
--- modules/module-mp-1/src/commonMain/kotlin/com/generic/modules/ModuleCommon1.kt
+++ modules/module-mp-1/src/commonMain/kotlin/com/generic/modules/ModuleCommon1.kt
@@ -1,7 +1,11 @@
 package com.generic.modules
 
 import com.generic.integrations.IntegrationJvm
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
+@JsExport
+@ExperimentalJsExport
 class ModuleCommon1 {
 
 	override fun toString(): String {
