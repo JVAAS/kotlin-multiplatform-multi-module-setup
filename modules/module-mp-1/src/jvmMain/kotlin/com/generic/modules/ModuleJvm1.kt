a0ed2ed26da17d95fe1ecca8a5b7c2d2d6f75682
vlad <jan@jvaas.com>
2020-07-03 13:42:20 +0200

    fix imports.

--- modules/module-mp-1/src/jvmMain/kotlin/com/generic/modules/ModuleJvm1.kt
+++ modules/module-mp-1/src/jvmMain/kotlin/com/generic/modules/ModuleJvm1.kt
@@ -1,11 +1,14 @@
 
 import com.generic.integrations.*
+import com.generic.modules.ModuleCommon1
+import kotlin.js.ExperimentalJsExport
 
 
 class ModuleJvm1 {
 
+	@ExperimentalJsExport
 	override fun toString(): String {
-		return ModuleCommon1Test().toString() + "::ModuleJvm1" + "::" + IntegrationJvm().toString()
+		return ModuleCommon1().toString() + "::ModuleJvm1" + "::" + IntegrationJvm().toString()
 	}
 
 
