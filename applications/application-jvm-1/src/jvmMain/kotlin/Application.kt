d79c90a1326a806121208a6ab7168398010dd137
vlad <jan@jvaas.com>
2020-07-04 15:04:07 +0200

    bring in CommonModule into Application.kt

--- applications/application-jvm-1/src/jvmMain/kotlin/Application.kt
+++ applications/application-jvm-1/src/jvmMain/kotlin/Application.kt
@@ -1,13 +1,22 @@
 package com.generic.applications
 
 import ModuleJvm1
+import com.generic.modules.ModuleCommon1
+import kotlin.js.ExperimentalJsExport
 
 object Application {
 
 	@JvmStatic
+	@ExperimentalJsExport
 	fun main(args: Array<String>) {
 
-		println("Application::" + ModuleJvm1())
+		println(
+			listOf(
+				"Application",
+				ModuleJvm1(),
+				ModuleCommon1()
+			).joinToString(separator = "::") { it.toString() }
+		)
 
 	}
 
