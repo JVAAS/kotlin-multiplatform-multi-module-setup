e319c05901e3ca8bdc6733626b9b5c607a74be14
vlad <jan@jvaas.com>
2020-07-05 15:49:34 +0200

    now adjusting typescript headers to be valid typescript.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,4 +21,44 @@ project("modules:module-mp-1") {
 	}
 }
 
+gradle.buildFinished {
+
+	// make some adjustments to typescript headers which are currently not valid typescript.
+	println("================================================================================")
+	println("Adjusting TypeScript Headers")
+	println("================================================================================")
+	allprojects.forEach { subproject ->
+
+		//println("==============================================================")
+		val path = "$buildDir/js/packages/${project.name}-${subproject.name}/kotlin/${project.name}-${subproject.name}.d.ts"
+		if (!File(path).exists()) {
+			return@forEach
+		}
+		println(path)
+		val newLines = mutableListOf<String>()
+		if (File(path).exists()) {
+			File(path).readLines().forEach { line ->
+				if (line.startsWith("declare namespace") || line.startsWith("}")) {
+					"// $line".let {
+						newLines.add(it)
+						//println(it)
+					}
+				} else if (line.trim().startsWith("namespace")) {
+					line.replace("namespace", "export namespace").let {
+						newLines.add(it)
+						//println(it)
+					}
+				} else {
+					line.let {
+						newLines.add(it)
+						//println(it)
+					}
+				}
+			}
+			File(path).writeText(newLines.joinToString("\n"))
+		}
+	}
+	println("================================================================================")
+}
+
 
